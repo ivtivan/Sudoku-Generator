9f3d7ca0c9f4c8381ede585e376e5c4dc1e20fb0
Ivan <48709677+ivtivan@users.noreply.github.com>
2023-04-04 23:31:11 +0300

    shuffling added

--- src/SudokuGridGenerator/SudokuGridGenerator.h
+++ src/SudokuGridGenerator/SudokuGridGenerator.h
@@ -2,15 +2,20 @@
 #define SUDOKU_GRID_GENERATOR_H
 
 #include <array>
+#include <map>
 #include "../helpers.h"
 
 class SudokuGridGenerator {
     private:
         std::array<uint8_t, SUDOKU_TILES> grid;
 
-        void init_grid() const;
+        void init_grid();
+        void reorder_correctly();
+
+        uint8_t get_at(uint8_t row, uint8_t col) const;
+        uint8_t& get_at(uint8_t row, uint8_t col);
     public:
-        std::array<uint8_t, SUDOKU_TILES> get_random_grid() const;
+        std::array<uint8_t, SUDOKU_TILES> get_random_grid();
 };
 
 #endif
